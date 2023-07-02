70fa9685156f924ee8b49dcb734f5cfd63047ce2
Harold <haroldquekhere@gmail.com>
2023-07-02 21:31:08 +0800

    adding multiple pages

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+export default function HomePage() {
+    return(
+        <>
+        <h1>Indie Gamer</h1>
+        <p>
+            Only the best Indie Games reviewed for you!
+        </p>
+        </>
+    )
+}
