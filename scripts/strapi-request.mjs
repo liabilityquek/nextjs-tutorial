2c67961c27837c3e12849eac1b39c379e81d50bf
Harold <haroldquekhere@gmail.com>
2023-07-23 19:50:34 +0800

    added strapi api

--- scripts/strapi-request.mjs
+++ scripts/strapi-request.mjs
@@ -1,8 +1,29 @@
-import { writeFileSync } from 'node:fs'
+import { writeFileSync } from "node:fs";
+import qs from "qs";
 
-const url = 'http://localhost:1337/api/reviews' + '?populate=*';
-const response = await fetch(url)
-const body = await response.json()
-const formatted = JSON.stringify(body, null, 2)
-const file = 'scripts/strapi-request.json'
-writeFileSync(file, formatted, 'utf8')
+const url =
+  "http://localhost:1337/api/reviews" +
+  "?" +
+  qs.stringify(
+    { //have to use fields based on strapi documentation
+      fields: ["slug", "title", "subtitle", "publishedAt"],
+      populate: {
+        image: {
+          fields: ["url"],
+        },
+        sort: ['publishedAt: desc'],
+        pagination: { pageSize: 6 },
+      },
+      //   populate: '*' //query all fields
+    },
+    {
+      encodeValuesOnly: true,
+    }
+  );
+
+console.log("url:", url);
+const response = await fetch(url);
+const body = await response.json();
+const formatted = JSON.stringify(body, null, 2);
+const file = "scripts/strapi-request.json";
+writeFileSync(file, formatted, "utf8");
