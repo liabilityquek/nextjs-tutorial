621bea4fac7aa969189dbe9df7694dd040b303b5
Harold <haroldquekhere@gmail.com>
2023-07-31 15:42:41 +0800

    added revalidateTag to replace revalidate:30s

--- app/webhooks/cms-event/route.js
+++ app/webhooks/cms-event/route.js
@@ -1,7 +1,13 @@
 import { NextResponse } from "next/server";
+import { revalidateTag } from 'next/cache'
+import { CACHE_TAG_REVIEWS } from "lib/reviews";
 
 export async function POST(request) {
   const payload = await request.json();
-  console.log('payload:', payload);
+  if (payload.model === 'review'){
+    revalidateTag(CACHE_TAG_REVIEWS)
+
+  }
+  console.log('revalidated:', CACHE_TAG_REVIEWS);
   return new Response(null, { status: 204 });
 }
