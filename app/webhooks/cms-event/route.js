bf4a8c69e19b6587151898f54bf2ec411b6ef923
Harold <haroldquekhere@gmail.com>
2023-08-01 15:00:17 +0800

    added nodemailer and sendgrid to have the webhook notifications to be send to an email

--- app/webhooks/cms-event/route.js
+++ app/webhooks/cms-event/route.js
@@ -1,14 +1,28 @@
 import { NextResponse } from "next/server";
 import { revalidateTag } from 'next/cache'
 import { CACHE_TAG_REVIEWS } from "lib/reviews";
+const sendEmail = require('../../../nodemailer/index.js');
+
 
 export async function POST(request) {
-  const payload = await request.json();
-  console.log(`payload: ${JSON.stringify(payload, null, 2)}`)
-  if (payload.model === 'review'){
-    revalidateTag(CACHE_TAG_REVIEWS)
+  try {
 
+    const payload = await request.json();
+    console.log(`payload: ${JSON.stringify(payload, null, 2)}`)
+    if (payload.model === 'review') {
+      revalidateTag(CACHE_TAG_REVIEWS)
+      await sendEmail({
+        email: process.env.EMAIL_FROM,
+        subject: `Game reviews - ${payload.event} for title: ${payload.title}`,
+        templateName: "email_notification",
+        templateVariables: payload
+      })
+
+    }
+    console.log('revalidated:', CACHE_TAG_REVIEWS);
+    return new Response(null, { status: 204 });
+  } catch (error) {
+    console.error('Error processing request:', error);
+    return new Response('Error processing request', { status: 500 });
   }
-  console.log('revalidated:', CACHE_TAG_REVIEWS);
-  return new Response(null, { status: 204 });
 }
---
+++ nodemailer/index.js
@@ -0,0 +1,37 @@
+const nodemailer = require("nodemailer");
+const ejs = require("ejs");
+const fs = require("fs");
+const path = require("path");
+
+export async function sendEmail(options) {
+    try {
+
+        const template = fs.readFileSync(
+            path.resolve(__dirname, `../email_template/${options.templateName}.html`),
+            "utf-8"
+        );
+
+        const html = ejs.render(template, options.templateVariables);
+        const transporter = nodemailer.createTransport({
+            host: process.env.EMAIL_HOST,
+            port: process.env.EMAIL_PORT,
+            auth: {
+                user: "apikey",
+                pass: process.env.SENDGRID_API_KEY,
+            },
+        });
+        const mailOptions = {
+            from: process.env.EMAIL_FROM,
+            // to: options.email,
+            to: process.env.EMAIL_TO,
+            subject: options.subject,
+            html: html,
+
+        };
+        //   console.log(mailOptions.html);
+        await transporter.sendMail(mailOptions);
+    } catch (error) {
+        console.error(`Error sending email notification: ${error}`)
+    }
+
+};
